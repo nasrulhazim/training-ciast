2c303665cb5559ad3e1695601cf166dcb2ea82e7
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2026-05-19 11:51:04 +0800

    Initial Commit

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,32 @@
+<x-layouts::app :title="__('Users')">
+    <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <table class="min-w-full divide-y divide-gray-200">
+                    <thead class="bg-gray-50">
+                        <tr>
+                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Name</th>
+                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">E-mail</th>
+                            <th class="px-6 py-3"></th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-white divide-y divide-gray-200">
+
+                        @forelse ($users as $user)
+                            <tr>
+                                <td class="px-6 py-4 text-sm font-medium text-gray-900">{{ $user->name }}</td>
+                                <td class="px-6 py-4 text-sm text-gray-500">{{ $user->email }}</td>
+                                <td class="px-6 py-4 text-right">
+                                    <a href="{{ route('users.show', $user) }}"
+                                       class="text-indigo-600 hover:text-indigo-900">View</a>
+                                </td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="3" class="px-6 py-8 text-center text-gray-500">No users yet.</td>
+                            </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
+    </div>
+</x-layouts::app>
